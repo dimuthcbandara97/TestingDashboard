cf4eca15c92e581d280284037f7ec4e94717fe12
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-13 19:06:19 +0530

    Base Structure Created

---
+++ TestingDashboard/Base/API/APIAuth.swift
@@ -0,0 +1,8 @@
+//
+//  APIAuth.swift
+//  TestingDashboard
+//
+//  Created by Dimuth Bandara on 2023-03-13.
+//
+
+import Foundation
---
+++ TestingDashboard/Base/Applications/Firebase/FirebaseAuth.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseAuth.swift
+//  TestingDashboard
+//
+//  Created by Dimuth Bandara on 2023-03-13.
+//
+
+import Foundation
