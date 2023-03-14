ed029b6c808dbfe877b787bd23f1388d3281cb65
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-14 08:49:36 +0530

    String resources added -> TabViewController, Navigation Contoller

--- TestingDashboard/Resources/Resources.swift
+++ TestingDashboard/Resources/Resources.swift
@@ -8,7 +8,35 @@
 import UIKit
 
 enum OverallController {
+    
+    // Create seperate enums for Strings, Images, Fonts, and Colors
     enum Strings {
+        // For TabBar Controller
+        enum TabBar {
+            static func title(for tab: Tabs) -> String {
+                switch tab {
+                case .dashboard: return "Exercise Dashboard"
+                case .session: return "Your Session"
+                case .progress: return "Your Progress"
+                case .settings: return "Settings"
+                }
+            }
+        }
+        // For NavBar Controller
+        enum NavBar {
+            static let dashboard = "Hello"
+            static let session = "Load Your Exercise"
+            static let progress = "Your Progress"
+            static let settings = "Settings"
+        }
+    }
+    enum Images {
+        
+    }
+    enum Fonts {
+        
+    }
+    enum Colors {
         
     }
 }
