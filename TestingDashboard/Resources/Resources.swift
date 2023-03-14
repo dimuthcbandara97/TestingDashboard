9e5bc8c89dee34378626b8bdbc26931b39a2ed12
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-14 08:59:45 +0530

    Image Names Added - > TabBarController (Overall)

--- TestingDashboard/Resources/Resources.swift
+++ TestingDashboard/Resources/Resources.swift
@@ -31,7 +31,17 @@ enum OverallController {
         }
     }
     enum Images {
-        
+        // For TabBar Contorller
+        enum TabBar {
+            static func icon(for tab: Tabs) -> UIImage? {
+                switch tab {
+                case .dashboard: return UIImage(systemName: "house")
+                case .session: return UIImage(systemName: "timer")
+                case .progress: return UIImage(systemName: "chart.bar")
+                case .settings: return UIImage(systemName: "gear")
+                }
+            }
+        }
     }
     enum Fonts {
         
