c342b2e3b271e06c4e06c2dd41bb6b3e7cadf1c9
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-15 09:58:38 +0530

    DashBoardNavBar (Basic)

--- TestingDashboard/Controllers/Dashboard/Views/DashboardNavBar.swift
+++ TestingDashboard/Controllers/Dashboard/Views/DashboardNavBar.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DashboardNavBar: UIView {
+class DashboardNavBar: BaseView {
     
     // title label
     private let titlelabel: UILabel = {
@@ -19,5 +19,79 @@ class DashboardNavBar: UIView {
     }()
     
     // Add Button
+    private let addButton = UIButton()
+    private let allWorkoutButton: BaseButton = {
+        let button = BaseButton(with: .secondary)
+        button.setTitle("My Workouts")
+        return button
+    }()
     
+    // layout subViews Here
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        //Addig for all subviews!
+        addBottomBorder(with:.systemBlue, height: 1)
+    }
+    
+    func addAllWorkoutsAction(_ action: Selector, with target: Any?) {
+        allWorkoutButton.addTarget(target, action: action, for: .touchUpInside)
+    }
+    
+    func addButtonAction(_ action: Selector, with target: Any?) {
+        addButton.addTarget(target, action: action, for: .touchUpInside)
+    }
 }
+
+
+// Extension of DashboardNavBar
+
+extension DashboardNavBar {
+   
+    override func setupViews() {
+        super.setupViews()
+        
+        setupView(allWorkoutButton)
+        setupView(titlelabel)
+        setupView(addButton)
+    }
+   
+    override func constaintViews() {
+        super.constaintViews()
+        
+        NSLayoutConstraint.activate([
+
+            addButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+
+            addButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+
+            addButton.heightAnchor.constraint(equalToConstant: 28),
+
+            addButton.widthAnchor.constraint(equalToConstant: 28),
+            
+            allWorkoutButton.topAnchor.constraint(equalTo: addButton.topAnchor),
+            allWorkoutButton.trailingAnchor.constraint(equalTo: addButton.leadingAnchor, constant: -15),
+            allWorkoutButton.heightAnchor.constraint(equalToConstant: 28),
+            allWorkoutButton.widthAnchor.constraint(equalToConstant: 130),
+            
+            titlelabel.centerYAnchor.constraint(equalTo: addButton.centerYAnchor),
+            titlelabel.trailingAnchor.constraint(equalTo: addButton.leadingAnchor),
+            titlelabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            
+        ])
+    }
+   
+    override func configureAppearance() {
+        super.configureAppearance()
+     
+        backgroundColor = .white
+
+        
+        allWorkoutButton.setTitle("All My workouts")
+        
+        addButton.setImage(UIImage(systemName: "home"), for: .normal)
+        
+      
+    }
+}
+
