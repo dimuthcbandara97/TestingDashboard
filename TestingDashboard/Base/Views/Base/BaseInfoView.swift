b394c52f36a34d89e0ea6288cb0e80164031a084
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-15 16:45:30 +0530

    BaseInfoView added

--- TestingDashboard/Base/Views/Base/BaseInfoView.swift
+++ TestingDashboard/Base/Views/Base/BaseInfoView.swift
@@ -9,14 +9,45 @@ import UIKit
 import Foundation
 
 class BaseInfoView: BaseView {
+    
     private let titleLabel: UILabel = {
         let label = UILabel()
-//        label.font
+//        label.font =
         label.textColor = .black
         return label
     }()
     
+    private let button = BaseButton(with: .primary)
     
+    let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+//        view.layer.borderColor = .red
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 5
+        return view
+    }()
+    
+    init(with title: String? = nil, buttonTitle: String? = nil) {
+        
+        // Title Label
+        titleLabel.text = title?.uppercased()
+        titleLabel.textAlignment = buttonTitle == nil ? .center : .left
+        
+        // Button
+        button.setTitle(buttonTitle?.uppercased())
+        button.isHidden = buttonTitle == nil ? true : false
+        
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(frame: .zero)
+    }
+    
+    func addButtonTarget(target: Any?, action: Selector) {
+        button.addTarget(action, action: action, for: .touchUpInside)
+    }
 }
 
 extension BaseInfoView {
@@ -24,6 +55,41 @@ extension BaseInfoView {
         super.setupViews()
         
         setupView(titleLabel)
-
+        setupView(button)
+        setupView(contentView)
+    }
+    
+    override func constaintViews() {
+        super.constaintViews()
+        
+        let contentTopAnchor: NSLayoutAnchor = titleLabel.text == nil ? topAnchor : titleLabel.bottomAnchor
+        let contentTopOffset: CGFloat = titleLabel.text == nil ? 0 : 10
+        
+        NSLayoutConstraint.activate([
+            
+            // Title Label
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            // Button
+            button.trailingAnchor.constraint(equalTo: trailingAnchor),
+            button.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            button.heightAnchor.constraint(equalToConstant: 28),
+            
+            // Content View
+            contentView.topAnchor.constraint(equalTo: contentTopAnchor, constant: contentTopOffset),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    override func configureAppearance() {
+        super.configureAppearance()
+        
+        backgroundColor = .clear
     }
 }
+
+
