d53d1421f2fe331a6d73635da39f23f7a0045ebb
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-16 11:15:07 +0530

    PercentView Added

--- TestingDashboard/Controllers/Session/Views/TimerView/PercentView.swift
+++ TestingDashboard/Controllers/Session/Views/TimerView/PercentView.swift
@@ -6,3 +6,67 @@
 //
 
 import Foundation
+import UIKit
+
+extension TimerView {
+    
+    final class PercentView: BaseView {
+         
+        // Stack View
+        private let stackView: UIStackView = {
+            let view = UIStackView()
+            view.axis = .vertical
+            view.distribution = .fillProportionally
+            view.spacing = 5
+            return view
+        }()
+        
+        // Percent
+        private let percentLabel: UILabel = {
+            let label = UILabel()
+//            label.font =
+            label.textColor = .black
+            label.textAlignment = .center
+            return label
+        }()
+        
+        // Subtitle
+        private let subtitleLabel: UILabel = {
+            let label = UILabel()
+//            label.font =
+            label.textColor = .black
+            label.textAlignment = .center
+            return label
+        }()
+
+        
+     // Setting Up
+    override func setupViews() {
+        super.setupViews()
+        setupView(stackView)
+        stackView.addArrangedSubview(percentLabel)
+        stackView.addArrangedSubview(subtitleLabel)
+    }
+     
+    // Constraint Views
+    override func constaintViews() {
+        
+        super.constaintViews()
+        
+        // Layouts
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+        
+        // Configuring
+        func configure(with title: String, andValue value: Int) {
+            subtitleLabel.text = title
+            percentLabel.text = "\(value)%"
+        }
+       
+    }
+}
