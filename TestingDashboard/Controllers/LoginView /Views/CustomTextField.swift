01622f7916efd596d6de13f8cabccc2ed15997d7
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-21 14:13:29 +0530

    Custom Text Field Added

---
+++ TestingDashboard/Controllers/LoginView /Views/CustomTextField.swift
@@ -0,0 +1,63 @@
+//
+//  CustomTextField.swift
+//  TestingDashboard
+//
+//  Created by Dimuth Bandara on 2023-03-21.
+//
+
+import Foundation
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    
+    // Defining username, email and password
+    enum CustomTextFieldType {
+        case username
+        case email
+        case password
+    }
+    
+    // defininf auth Field Type
+    private let authFieldType: CustomTextFieldType
+    
+    init(fieldType: CustomTextFieldType) {
+        self.authFieldType = fieldType
+        super.init(frame: .zero)
+        
+        self.backgroundColor = .secondarySystemBackground
+        self.layer.cornerRadius = 10
+        
+        self.returnKeyType = .done
+        self.autocorrectionType = .no
+        self.autocapitalizationType = .none
+        
+        self.leftViewMode = .always
+        self.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: self.frame.size.height))
+        
+        switch fieldType {
+        
+        // user name
+        case .username:
+            self.placeholder = "Username"
+        
+        // email
+        case .email:
+            self.placeholder = "Email Address"
+            self.keyboardType = .emailAddress
+            self.textContentType = .emailAddress
+            
+        // password
+        case .password:
+            self.placeholder = "Password"
+            self.textContentType = .oneTimeCode
+            self.isSecureTextEntry = true
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
