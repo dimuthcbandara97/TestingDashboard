59e86f2323c751f759cebf282604a9a7b9f2291f
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-26 16:12:40 +0530

    Progress Element Added

---
+++ TestingDashboard/Base/API/Element/ProgressElement.swift
@@ -0,0 +1,26 @@
+//
+//  ProgressElement.swift
+//  TestingDashboard
+//
+//  Created by Dimuth Bandara on 2023-03-26.
+//
+
+import Foundation
+
+// MARK: - ProgressElement
+struct ProgressElement: Codable {
+    let id, progressName: String
+    let dailyCount: Int
+    let date: String
+    let month, v: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case progressName = "progress_name"
+        case dailyCount = "daily_count"
+        case date, month
+        case v = "__v"
+    }
+}
+
+typealias Progress = [ProgressElement]
