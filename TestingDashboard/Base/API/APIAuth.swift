fb7cd2892c165694c056a426d47eaf0a8537a110
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-26 17:11:50 +0530

    APIAUth -> Progress (Insert) added

--- TestingDashboard/Base/API/APIAuth.swift
+++ TestingDashboard/Base/API/APIAuth.swift
@@ -194,6 +194,36 @@ class APICaller {
         task.resume()
     }
     
+    // CRUDProgress - Insert
+
+    func insertUser(user: ProgressElement) {
+        let url = URL(string: Constants.API_KEY_PROGRESS)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let encoder = JSONEncoder()
+        do {
+            let jsonData = try encoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print(error.localizedDescription)
+            return
+        }
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                print("API error")
+                return
+            }
+            // Update UI with new data
+        }
+        task.resume()
+    }
+    
     // CRUD Timer -> Read
     func loadTimer(completion: @escaping ([TimerElement]) -> Void){
            
