f73913527aefda1a6900cd4c1a7fb23c2038da41
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-05-10 16:05:07 +0530

    Wrapper Class Added

--- TestingDashboard/Controllers/Dashboard/CustomSchedulePage.swift
+++ TestingDashboard/Controllers/Dashboard/CustomSchedulePage.swift
@@ -52,6 +52,7 @@
 import Foundation
 import UIKit
 import CalendarKit
+//import CalendeKitUI
 import EventKit
 
 class CustomSchedulePage: DayViewController {
@@ -63,6 +64,7 @@ class CustomSchedulePage: DayViewController {
         view.backgroundColor = .red
         title = "Custom Schedule Page"
         requestAccessToCalendar()
+        subscribeToNotifications()
     }
     
     func requestAccessToCalendar() {
@@ -71,18 +73,26 @@ class CustomSchedulePage: DayViewController {
         }
     }
     
+    func subscribeToNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(storeChanged(_:)), name: .EKEventStoreChanged, object: nil)
+    }
+    
+    @objc func storeChanged(_ notification: Notification){
+        reloadData()
+    }
+    
     override func eventsForDate(_ date: Date) -> [EventDescriptor] {
         let startDate = date
         var oneDayComponents = DateComponents()
         oneDayComponents.day = 1
         
-        let endDate = Calendar.current.date(byAdding: oneDayComponents, to: startDate)!
+        let endDate = calendar.date(byAdding: oneDayComponents, to: startDate)!
         
         let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
         
         let eventKitEvents = eventStore.events(matching: predicate)
         
-        let calendarKitEvents = eventKitEvents.map { ekEvent in
+        let calendarKitEvents = eventKitEvents.map { ekEvent -> Event in
             let ckEvent = Event()
             ckEvent.startDate = ekEvent.startDate
             ckEvent.endDate = ekEvent.endDate
@@ -93,8 +103,11 @@ class CustomSchedulePage: DayViewController {
             }
             return ckEvent
         }
-        
         return calendarKitEvents
+    }
+    
+    override func dayViewDidSelectEventView(_ eventView: EventView) {
+        print("Event Clicked")
     }
 }
 
